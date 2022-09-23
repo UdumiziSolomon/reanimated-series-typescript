54b8e379246be908f17882a9d09f1512ccd672f5
pragma <solomonudumizi@gmail.com>
2022-09-23 18:42:43 +0100

    REANIMATED-09.

--- App.tsx
+++ App.tsx
@@ -37,7 +37,7 @@ const App = () => {
           {/* <PinchGesture /> */}
           {/* <SingleAndDoubleTap /> */}
           {/* <PanGestureScroll /> */}
-          <ColorPicker />
+          {/* <ColorPicker /> */}
 
         </GestureHandlerRootView>
 
